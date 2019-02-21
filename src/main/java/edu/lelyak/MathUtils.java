d0059020b0d6c50c169472b9d38bdaf47f135a7b
Nazar Lelyak <nazarlelyak@seniordevonly.com>
2019-02-21 20:43:46 +0200

    Initial commit

---
+++ src/main/java/edu/lelyak/MathUtils.java
@@ -0,0 +1,23 @@
+package edu.lelyak;
+
+/**
+ * @author Nazar_Lelyak.
+ */
+public class MathUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/edu/lelyak/MathUtilsTest.java
@@ -0,0 +1,16 @@
+package edu.lelyak;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Nazar_Lelyak.
+ */
+class MathUtilsTest {
+
+    @Test
+    void test() {
+        System.out.println("This is test run");
+    }
+}
