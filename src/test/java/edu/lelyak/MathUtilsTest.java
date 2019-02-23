f1eaa4917548178f6c6c685fa6d92feb8f77fddf
Nazar Lelyak <nazarlelyak@seniordevonly.com>
2019-02-23 12:12:29 +0200

    add assertAll()

--- src/test/java/edu/lelyak/MathUtilsTest.java
+++ src/test/java/edu/lelyak/MathUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -59,5 +60,14 @@ class MathUtilsTest {
             assertEquals( 0, mathUtils.add(-1 , 1),
                     "Add method should return the sum of two numbers");
         }
+    }
+
+    @Test
+    void testMultiply() {
+        assertAll(
+                () -> assertEquals( 0, mathUtils.multiply(1, 0)),
+                () -> assertEquals( 1, mathUtils.multiply(1, 1)),
+                () -> assertEquals( 6, mathUtils.multiply(2, 3))
+        );
     }
 }
